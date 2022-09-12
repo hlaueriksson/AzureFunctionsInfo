c114067e7ec5444ede1af3d4acf6acf0095b7fdd
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2022-09-12 19:10:56 +0200

    AzureFunctionsInfoV7

---
+++ AzureFunctionsInfoV7/Assemblies.cs
@@ -0,0 +1,30 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionsInfoV7
+{
+    public class Assemblies
+    {
+        private readonly ILogger _logger;
+
+        public Assemblies(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Assemblies>();
+        }
+
+        [Function("Assemblies")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var result = assemblies.Except(new[] { typeof(Assemblies).Assembly }).OrderBy(x => x.FullName).Select(x => x.ToString()).ToList();
+
+            _logger.LogInformation("{Count} assemblies found", assemblies.Length);
+
+            var response = req.CreateResponse();
+            response.WriteAsJsonAsync(result);
+
+            return response;
+        }
+    }
+}
